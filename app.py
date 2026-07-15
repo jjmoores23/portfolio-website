a983558cd2dc6dcc800d15340c4194f4c1bf123f
Jacob Moores <90861314+jjmoores23@users.noreply.github.com>
2026-07-15 12:04:00 -0230

    Refactor app.py to improve structure and readability

--- app.py
+++ app.py
@@ -517,4 +517,5 @@ def handle_exception(error: Exception):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", "5000"))
+    app.run(host="0.0.0.0", port=port)
